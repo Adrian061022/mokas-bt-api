084c205508bd0ebe1edd41f2806eb317072ece5c
Adrian061022 <markusadrian2006@gmail.com>
2026-02-16 09:25:52 +0100

    kész

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Task::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:függőben,folyamatban,befejezett',
+        ]);
+
+        $task = Task::create($validated);
+
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+
+        $task = Task::findOrFail($id);
+
+        $validated = $request->validate([
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'sometimes|required|in:függőben,folyamatban,befejezett',
+        ]);
+
+        $task->update($validated);
+
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        Task::destroy($id);
+        return response()->json(['message' => 'Task deleted successfully'], 200);
+
+    }
+}
---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Task::factory()->count(3)->create();
+        $tasks = [
+            [
+                'title' => 'Első feladat',
+            ],
+            [
+                'title' => 'Második feladat',
+                'description' => 'Ez a második feladat leírása.',
+                'status' => 'folyamatban',
+            ],
+           
+        ];
+
+        foreach ($tasks as $task) {
+            Task::create($task);
+        }
+    }
+}
